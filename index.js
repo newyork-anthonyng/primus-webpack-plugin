94ebd3f2860c750d9c871f1666f645e446a35d8f
Alexandre Nicolas <alex29820@gmail.com>
2018-12-03 11:01:02 +0100

    Add webpack 4 support

--- index.js
+++ index.js
@@ -3,74 +3,68 @@ const assert = require('assert');
 const Primus = require('primus');
 const uglify = require('uglify-js');
 
-class PrimusWebpackPlugin {
-  constructor(options) {
+function PrimusWebpackPlugin(options) {
     this.options = Object.assign(
-      {},
-      {
-        filename: 'primus-client.js',
-        minify: false,
-        primusOptions: {},
-      },
-      options
+        {},
+        {
+          filename: 'primus-client.js',
+          minify: false,
+          primusOptions: {},
+        },
+        options
     );
-  }
+}
 
-  apply(compiler) {
-    compiler.plugin('emit', (compilation, cb) => {
-      const primus = new Primus(new EventEmitter(), this.options.primusOptions);
+PrimusWebpackPlugin.prototype.apply = function(compiler) {
 
-      if (this.options.primusOptions.plugins) {
-        this.options.primusOptions.plugins.forEach(plugin => {
-          assert(plugin.name, 'Plugin must have name!');
-          assert(plugin.plugin, 'Plugin must have plugin!');
+    compiler.hooks.compilation.tap('emit', (compilation, cb) => {
+        const primus = new Primus(new EventEmitter(), this.options.primusOptions);
 
-          primus.plugin(plugin.name, plugin.plugin);
-        });
-      }
+        if (this.options.primusOptions.plugins) {
+          this.options.primusOptions.plugins.forEach(plugin => {
+            assert(plugin.name, 'Plugin must have name!');
+            assert(plugin.plugin, 'Plugin must have plugin!');
 
-      const clientLib = primus.library();
-      const filename = this.options.filename.replace(
-        '[hash]',
-        compilation.hash
-      );
-      const source = this.options.minify
-        ? uglify.minify(clientLib, { fromString: true })
-        : { code: clientLib };
-
-      compilation.assets[filename] = {
-        source() {
-          return source.code;
-        },
-        size() {
-          return source.code.length;
-        },
-      };
-
-      primus.destroy();
-      cb(null);
-    });
-
-    // if HtmlWebpackPlugin is being utilized, add our script to file
-    compiler.plugin('compilation', compilation => {
-      compilation.plugin(
-        'html-webpack-plugin-before-html-generation',
-        (htmlPluginData, cb) => {
-          const filename = this.options.filename.replace(
-            '[hash]',
-            compilation.hash
-          );
-          const publicPath = compilation.outputOptions.publicPath || "";
-
-          // We are putting Primus script before other JavaScript files
-          // because we are expecting other bundles to use Primus
-          htmlPluginData.assets.js.unshift(`${publicPath}${filename}`)
-
-          cb(null, htmlPluginData);
+            primus.plugin(plugin.name, plugin.plugin);
+          });
         }
-      );
+
+        const clientLib = primus.library();
+        const filename = this.options.filename.replace(
+          '[hash]',
+          compilation.hash
+        );
+        const source = this.options.minify
+          ? uglify.minify(clientLib, { fromString: true })
+          : { code: clientLib };
+
+        compilation.assets[filename] = {
+          source() {
+            return source.code;
+          },
+          size() {
+            return source.code.length;
+          },
+        };
+
+        primus.destroy();
     });
-  }
+
+    compiler.hooks.compilation.tap('htmlWebpackPluginBeforeHtmlGeneration', compilation => {
+        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('PrimusWebpackPlugin', (data, cb) => {
+            const filename = this.options.filename.replace(
+                '[hash]',
+                compilation.hash
+            );
+            const publicPath = compilation.outputOptions.publicPath || "";
+
+
+            // We are putting Primus script before other JavaScript files
+            // because we are expecting other bundles to use Primus
+            data.assets.js.unshift(`${publicPath}${filename}`)
+            cb(null, data);
+        })
+    });
 }
 
 module.exports = PrimusWebpackPlugin;
